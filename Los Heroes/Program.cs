7cd25ede565a1fe0f2eef837dae862ab3b915a28
sigmotoa <si.galvismotoa@gmail.com>
2024-04-01 16:40:15 -0500

    Heroe v_1 done

---
+++ Los Heroes/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var superMan = new Heroe();
+superMan.Id = 1;
+superMan.Nombre = "SuperMan";
+superMan.IdentidadSecreta = "Clark Kent";
+superMan.Ciudad = "Metropolis";
+superMan.Poderes = 
+    new[] {
+        "Super Fuerza", 
+        "Rayo Laser", 
+        "Super Salto", 
+        "Super Velocidad" };
+superMan.Volar = true;
+
+superMan.MostrarHeroe();
+
+class Heroe
+{
+    public int Id;
+    public string Nombre;
+    public string IdentidadSecreta;
+    public string Ciudad;
+    public string[] Poderes;
+    public bool Volar;
+
+    public void MostrarHeroe()
+    {
+        Console.WriteLine($"{Id}" +
+                          $"\n{Nombre}" +
+                          $"\n{IdentidadSecreta}" +
+                          $"\n{Ciudad}" +
+                          $"\nVolar {Volar}");
+    }
+}
